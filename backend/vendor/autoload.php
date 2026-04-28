c129672e730efadd404fcfdedb18b9ddd415976a
Maria Ruffo <maria.ruffo483@gmail.com>
2026-04-28 09:29:43 +0000

    backend

--- backend/vendor/autoload.php
+++ backend/vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2d3eba4e5aa500e6b7cb54ffc11e82ea::getLoader();
+return ComposerAutoloaderInit53b5d56b3b7e3cbac1713e68c8850f6c::getLoader();
